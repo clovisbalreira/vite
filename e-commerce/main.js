e38f162374506fc74ae9b27430ac08b5964eb584
Clóvis Balreira Rodrigues <62964663+clovisbalreira@users.noreply.github.com>
2023-09-03 08:46:02 -0300

    Update main.js

--- e-commerce/main.js
+++ e-commerce/main.js
@@ -1,11 +1,3 @@
-/* 
-    Codigo um aula : JS2023
-    Codigo dois aula : CSS2908
-    Codigo Tres aula : MAGAZINEHASH
-    Codigo Quadro aula : INTENSIVOJS15
-    Codigo cinco aula : github10
-*/
-
 import { renderizarCatalogo } from "./src/cartaoProduto";
 import { inicialiizarFiltros } from "./src/filtrosCatalogo";
 import { inicializarCarrinho, renderizarProdutosCarrinho, atualizarPrecoCarrinho } from "./src/menuCarrinho";
